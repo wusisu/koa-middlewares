312337fe9de189036f264b185f0226954d0f658d
吴思稣 <i@wusisu.com>
2016-06-24 11:53:01 +0800

    add a list of middlewares for koa

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # koa-middleware-package
-A package of middlewares for koa (@2)
+A package of middlewares for koa
+
+This is **JUST** a package of koa middlewares on npm and github.
+
+##Usage
+```js
+import {
+  responseTime,
+  compress,
+  conditionalGet,
+  etag,
+  favicon,
+  cors,
+  static,
+  router,
+  bodyparser,
+  views,
+} from 'koa-middleware-package'
+```
+
+## package list
+here are middlewares of the package, sorted in the order we use them.
+- [koa-response-time](https://github.com/koajs/response-time)
+- [Koa Compress](https://github.com/koajs/compress)
+- [koa-conditional-get](https://github.com/koajs/conditional-get)
+- [koa-etag](https://github.com/koajs/etag)
+- [koa-favicon](https://github.com/koajs/favicon)
+- [kcors](https://github.com/koajs/cors)
+- [koa-morgan](https://github.com/koa-modules/morgan)
+- [koa-static](https://github.com/koajs/static)
+- [koa-router](https://github.com/alexmingoia/koa-router)
+- [koa-bodyparser](https://github.com/koajs/bodyparser)
+- [koa-views](https://github.com/queckezz/koa-views)
---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.views = exports.bodyparser = exports.router = exports.static = exports.morgan = exports.cors = exports.favicon = exports.etag = exports.conditionalGet = exports.compress = exports.responseTime = undefined;
+
+var _koaResponseTime = require('koa-response-time');
+
+var _koaResponseTime2 = _interopRequireDefault(_koaResponseTime);
+
+var _koaCompress = require('koa-compress');
+
+var _koaCompress2 = _interopRequireDefault(_koaCompress);
+
+var _koaConditionalGet = require('koa-conditional-get');
+
+var _koaConditionalGet2 = _interopRequireDefault(_koaConditionalGet);
+
+var _koaEtag = require('koa-etag');
+
+var _koaEtag2 = _interopRequireDefault(_koaEtag);
+
+var _koaFavicon = require('koa-favicon');
+
+var _koaFavicon2 = _interopRequireDefault(_koaFavicon);
+
+var _kcors = require('kcors');
+
+var _kcors2 = _interopRequireDefault(_kcors);
+
+var _koaMorgan = require('koa-morgan');
+
+var _koaMorgan2 = _interopRequireDefault(_koaMorgan);
+
+var _koaStatic = require('koa-static');
+
+var _koaStatic2 = _interopRequireDefault(_koaStatic);
+
+var _koaRouter = require('koa-router');
+
+var _koaRouter2 = _interopRequireDefault(_koaRouter);
+
+var _koaBodyparser = require('koa-bodyparser');
+
+var _koaBodyparser2 = _interopRequireDefault(_koaBodyparser);
+
+var _koaViews = require('koa-views');
+
+var _koaViews2 = _interopRequireDefault(_koaViews);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.responseTime = _koaResponseTime2.default;
+exports.compress = _koaCompress2.default;
+exports.conditionalGet = _koaConditionalGet2.default;
+exports.etag = _koaEtag2.default;
+exports.favicon = _koaFavicon2.default;
+exports.cors = _kcors2.default;
+exports.morgan = _koaMorgan2.default;
+exports.static = _koaStatic2.default;
+exports.router = _koaRouter2.default;
+exports.bodyparser = _koaBodyparser2.default;
+exports.views = _koaViews2.default;
+
